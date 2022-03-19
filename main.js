8609998d744ed66eb4e18175b8838d5c42cd4b15
Andrea Strnadlova <an.strnadlova@gmail.com>
2022-03-19 21:26:25 +0100

    vyvolaná jména na začátek, počítadlo

---
+++ main.js
@@ -0,0 +1,67 @@
+let jmena = [
+    "Markéta",
+    "Kateřina",
+    "Júlia",
+    "Lucie",
+    "Aneta",
+    "Veronika",
+    "Petra",
+    "Olga",
+    "Naďa",
+    "Nikola",
+    "Eva",
+    "Anísa",
+    "Terezia",
+    "Diana",
+    "Anna",
+    "Iva",
+    "Denisa",
+    "Zuzana",
+    "Jana",
+    "Martina",
+    "Lenka",
+    "Iveta",
+    "Pavla",
+    "Lucie",
+    "Kamila",
+    "Soňa"
+];
+
+let tazenaJmena = [];
+
+function tahnoutJmeno() {
+    if (jmena.length === 0) {
+        return;
+    }
+
+    // Generujeme náhodný index
+    let vyherniIndex = Math.floor(Math.random() * jmena.length);
+
+    // Získáme výherní jméno na patřičném indexu
+    let vyherniJmeno = jmena[vyherniIndex];
+
+    // Vyřadíme vylosované jméno z osudí
+    jmena.splice(vyherniIndex, 1);
+
+    // Výherní jméno si uložíme do pole k ostatním výherním
+    tazenaJmena.unshift(vyherniJmeno);
+
+    let vyherce = document.querySelector('#vyherka');
+    vyherce.textContent = vyherniJmeno;
+
+    let seznam = document.querySelector('#seznam');
+    //seznam = tazenaJmena.unshift();
+    seznam.textContent = tazenaJmena;
+
+    pocitadlo();
+
+}
+
+function pocitadlo() {
+let pocetVyvolanych = document.querySelector("#pocet");
+pocetVyvolanych.innerHTML = tazenaJmena.length;
+}
+
+
+
+    
